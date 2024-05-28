38ef66c18caff5c4e730683fd9c5b9a90d192b7f
iwu_78 <keledang777@gmail.com>
2024-05-28 07:46:48 -0700

    fixed urls

---
+++ _site/js/consts.js
@@ -0,0 +1,4 @@
+const baseurl = "http://127.0.0.1:8787/api/"
+// const baseurl = "https://buttons.stu.nighthawkcodingsociety.com/api/"
+const websiteurl = "http://127.0.0.1:4000/"
+// const websiteurl = "https://iwu78.github.io/PBL_Frontend/"
